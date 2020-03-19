ea6f0e4c06333359ffddd6c618605e01f6c81d22
cherifmezdariADP <62327983+cherifmezdariADP@users.noreply.github.com>
2020-03-19 09:46:07 +0100

    some stuff

--- src/components/screens/Register/Register.js
+++ src/components/screens/Register/Register.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./styles.css";
-import Container from "./components/common/authentication_container/Container";
-
+import Container from "../../common/authentication_container/Container";
 export default function Register() {
   return (
     <div className="App">
