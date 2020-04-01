24d3f05f5263b956d65d0efb9391f7d537c86ea9
cherifmezdariADP <62327983+cherifmezdariADP@users.noreply.github.com>
2020-04-01 17:36:49 +0100

    FIXING FIREBASE

--- src/FireBase.js
+++ src/FireBase.js
@@ -1,7 +1,7 @@
 import * as firebase from "firebase";
 import "firebase/auth";
 
-const app = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyB_N8kqa-E2TFqnkrAdTFnB7OzYtRxG4kc",
   authDomain: "marketplaceapicertification.firebaseapp.com",
   databaseURL: "https://marketplaceapicertification.firebaseio.com",
@@ -10,5 +10,5 @@ const app = {
   messagingSenderId: "723560707286",
   appId: "1:723560707286:web:3b23e1fc9f28e2af248577",
   measurementId: "G-4KCCPZNGDQ"
-};
+});
 export default app;
